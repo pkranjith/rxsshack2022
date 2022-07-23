559f98f63a1bee112ee481e4ceddb161e8d265eb
divya <divyabattu09@gmail.com>
2022-07-23 18:57:09 -0500

    initial commit2

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
-    return <h1> HIIIII </h1>;
+    return (
+        <div>
+            <img src={"logo.png"} style={{width : "100%"}}/>
+
+
+
+            <div style={{marginLeft : "100px"}}>
+
+                <div style = {{width : "250px",
+            backgroundColor: 'yellow',
+            height: '375px'}}>
+            <div style={{width : "100%", height : "75%"}}>
+
+            </div>
+            <img src={"logo.png"} style={{width : "100%", height : "25%", borderTopLeftRadius : 150 / 2}}/>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
 }
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
