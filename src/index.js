1c2cdd365e777d9af31ccfed717e81164f59cbac
Ranjith <pkranjithpk@gmail.com>
2022-07-23 21:48:57 -0500

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,51 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import Background from './background.png';
+import Background from './background2.png';
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
     return (
         <div>
-            <img src={"logo.png"} style={{width : "100%"}}/>
+            <img src={"logo2.png"} style={{width: "100%"}}/>
+
+            <div>
+                <div className="bg_image"
+                    style={{
+                        backgroundImage: 'url(' + Background + ')',
+                        backgroundSize: "cover",
+                        height: "100vh"
+                    }}>
+
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <div className="coffeeDiv box-shadow">
+                                    <div style={{width: "100%", height: "60%"}}>
+                                        <text className={"catchUpHeading"}>
+                                            Chat over Coffee!
+                                        </text>
+                                        <text className={"catchUpText"}>
+                                            Make the next steps easier! Get to know your peers and build your network.
+                                        </text>
+
+                                    </div>
+
+                                    <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <div className="coffeeDiv box-shadow">
+                                    <div style={{width: "100%", height: "60%"}}>
+                                        <text className={"catchUpHeading"}>
+                                            Hangout with the team!
+                                        </text>
+                                        <text className={"catchUpText"}>
+                                            Schedule happy hours and bond with the team over fun games, activities and much more!
+                                        </text>
+
+                                    </div>
+
+                                    <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                </div>
+                            </div>
+
+                            <div className="col-md-3">
+                                <div className="coffeeDiv box-shadow">
+                                    <div style={{width: "100%", height: "60%"}}>
+                                        <text className={"catchUpHeading"}>
+                                            Hangout with the team!
+                                        </text>
+                                        <text className={"catchUpText"}>
+                                            Schedule happy hours and bond with the team over fun games, activities and much more!
+                                        </text>
+
+                                    </div>
+
+                                    <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                </div>
+                            </div>
+
+                            <div className="col-md-3">
+                                <div className="coffeeDiv box-shadow">
+                                    <div style={{width: "100%", height: "60%"}}>
+                                        <text className={"catchUpHeading"}>
+                                            Hangout with the team!
+                                        </text>
+                                        <text className={"catchUpText"}>
+                                            Schedule happy hours and bond with the team over fun games, activities and much more!
+                                        </text>
+
+                                    </div>
+
+                                    <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                </div>
+                            </div>
 
 
-<div>
-    <div
-        className="bg_image"
-        style={{
-            backgroundImage: 'url(' + Background + ')',
-            backgroundSize: "cover",
-            height: "100vh"
-        }}>
-
-        <div style={{paddingLeft : "100px"}}>
-        <div>
-            <div className={"coffeeDiv"}>
-                <div style={{width : "100%", height : "60%"}}>
-                    <text className={"catchUpHeading"} >
-                        Catch up over Coffee!
-                    </text>
-                    <text className={"catchUpText"} >
-                        Make the next steps easier! Get to know your peers and build your network.
-                    </text>
-
+                        </div>
+                    </div>
                 </div>
-
-                <img className={"coffeeImg"} src={"coffee.jpg"} />
             </div>
-
         </div>
-        </div>
-
-
-
-    </div>
-</div>
-</div>
     )
 }
+
 root.render(
-    <App />
+    <App/>
 );
