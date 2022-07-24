ac787421a605549668e2132ecdaa7e3ef5e1b1ec
Ranjith <pkranjithpk@gmail.com>
2022-07-24 03:03:22 -0500

    added camera

--- src/HappyHour.js
+++ src/HappyHour.js
@@ -22,9 +22,9 @@ class HappyHour extends React.Component {
                 flexWrap: "wrap"
             }}>
                <div>
-                   <iframe width="1200" height="900" src="https://telnyx-meet-demo.vercel.app/rooms/1535b4cf-7076-4aa8-aa5b-b74fa0265029?client_toke[…]LxwloSIzigWKWGAiIFrfIzpIq5B80me8V13QI9FbEUYOThwUDgzzFCQ"
+                   <iframe width="1200" height="900" src="https://telnyx-meet-demo.vercel.app/rooms/1535b4cf-7076-4aa8-aa5b-b74fa0265029?client_token=eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ0ZWxueXhfY2xpZW50X3Rva2VuIiwiZXhwIjoxNjU4NjUyNzc0LCJncmFudHMiOlt7ImFjdGlvbnMiOlsiam9pbiJdLCJyZXNvdXJjZXMiOlsidGVsbnl4OnZpZGVvOnJvb21zOjE1MzViNGNmLTcwNzYtNGFhOC1hYTViLWI3NGZhMDI2NTAyOSJdLCJzdWJqZWN0cyI6WyJ0ZWxueXg6dXNlcnM6ZWFjODU3ZTQtNDgzYS00YTQ2LWFmYzItZDk4Yzg4ZjkwOTY3Il19XSwiZ3JhbnRzX3ZlcnNpb24iOiIxLjAuMCIsImlhdCI6MTY1ODY0OTE3NCwiaXNzIjoidGVsbnl4X2NsaWVudF90b2tlbiIsImp0aSI6ImZiZjgxNTYyLTUwZWUtNGE0OS04NTYzLTZiYTFhYjMyM2IyMiIsIm5iZiI6MTY1ODY0OTE3Mywic3ViIjoibnVsbCIsInR5cCI6ImFjY2VzcyJ9.yEjsk0ed_2fz9kospKWdPtJeJTRFR7lXlpD5EmwreShtK_4DfZ9t5W1b3J5oFKgk5ilWkFbaGAsj1kTlTTauBA&refresh_token=eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ0ZWxueXhfY2xpZW50X3Rva2VuIiwiZXhwIjoxNjU4NzM1NTc0LCJncmFudHMiOlt7ImFjdGlvbnMiOlsiam9pbiJdLCJyZXNvdXJjZXMiOlsidGVsbnl4OnZpZGVvOnJvb21zOjE1MzViNGNmLTcwNzYtNGFhOC1hYTViLWI3NGZhMDI2NTAyOSJdLCJzdWJqZWN0cyI6WyJ0ZWxueXg6dXNlcnM6ZWFjODU3ZTQtNDgzYS00YTQ2LWFmYzItZDk4Yzg4ZjkwOTY3Il19XSwiZ3JhbnRzX3ZlcnNpb24iOiIxLjAuMCIsImlhdCI6MTY1ODY0OTE3NCwiaXNzIjoidGVsbnl4X2NsaWVudF90b2tlbiIsImp0aSI6IjgwMjZlYTU0LWE2ZDYtNDc1OS1iZTQwLWMyNGMyNjdlMmM3MiIsIm5iZiI6MTY1ODY0OTE3Mywic3ViIjoibnVsbCIsInR5cCI6InJlZnJlc2gifQ.bD3ckO_kmAHdXK1hUlcMu-NO-pGxI4fgBd7TKtpcJXo_mkM5G44MOIK0tlCaowMoZ08uO2zLa4_HIWnLRIgICQ"
                            title="YouTube video player" frameBorder="0"
-                           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; camera; microphone"
                            allowFullScreen></iframe>
                </div>
                 <div style={{paddingLeft: "20px", width: "10px"}}>
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css'
 import Background from './background2.png';
 
@@ -7,6 +7,7 @@ import Background from './background2.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class LandingPage extends React.Component {
 
@@ -18,24 +19,30 @@ class LandingPage extends React.Component {
         return (
             <div>
                 <div className="header" style={{
-                    marginLeft: "50px",
                     marginRight: "10px",
                     width: "100%",
-                    marginBottom: "150px"
+                    marginBottom: "150px",
+                    backgroundColor: "#ECF0F1"
                 }}>
                     <div className="row">
                         <div className="col-md-3">
-                            <img src={"logo3.png"} style={{
+                            <img src={"logo3.png"} alt="logo" style={{
                                 width: "225px",
                                 height: "150px"
                             }}/>
                         </div>
-                        <div className="col-md-6"></div>
-                        <div className="col-md-3">
-                            <div className="headerText">
+                        <div className="col-md-7"></div>
+                        <div className="col-md-1">
+                            <div className="headerText headerRight">
                                 Hi, Jane
                             </div>
                         </div>
+                        <div className="col-md-1">
+                            <div className="headerRight">
+                                <i class="fa fa-user fa-5x"></i>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
@@ -54,7 +61,7 @@ class LandingPage extends React.Component {
                                     <div className="coffeeDiv box-shadow">
                                         <div style={{width: "100%", height: "60%"}}>
                                             <text className={"catchUpHeading"}>
-                                                Chat over Coffee!
+                                                Chat over Coffee
                                             </text>
                                             <text className={"catchUpText"}>
                                                 Make the next steps easier! Get to know your peers and build your
@@ -68,18 +75,17 @@ class LandingPage extends React.Component {
 
                                         </div>
 
-                                        <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                        <img className={"coffeeImg"} alt="coffee" src={"coffee.jpg"}/>
                                     </div>
                                 </div>
                                 <div className="col-md-3">
                                     <div className="coffeeDiv box-shadow">
                                         <div style={{width: "100%", height: "60%"}}>
                                             <text className={"catchUpHeading"}>
-                                                Hangout with the team!
+                                                Hangout with the team
                                             </text>
                                             <text className={"catchUpText"}>
-                                                Schedule happy hours and bond with the team over fun games, activities
-                                                and much more!
+                                                Schedule happy hours and bond with the team over fun games and activities!
                                             </text>
                                             <button className={"coffeeButton"} onClick={this.onClickWellness} theme="pink">
                                                 Get Started
@@ -87,7 +93,7 @@ class LandingPage extends React.Component {
 
                                         </div>
 
-                                        <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                        <img className={"coffeeImg"} alt="happy" src={"happyHours.jpg"}/>
                                     </div>
                                 </div>
 
@@ -95,11 +101,10 @@ class LandingPage extends React.Component {
                                     <div className="coffeeDiv box-shadow">
                                         <div style={{width: "100%", height: "60%"}}>
                                             <text className={"catchUpHeading"}>
-                                                Hangout with the team!
+                                                Walk-in to Demos
                                             </text>
                                             <text className={"catchUpText"}>
-                                                Schedule happy hours and bond with the team over fun games, activities
-                                                and much more!
+                                                Join Demo sessions and learn about the newest products your peers are building.
                                             </text>
                                             <button className={"coffeeButton"} theme="pink">
                                                 Get Started
@@ -107,7 +112,7 @@ class LandingPage extends React.Component {
 
                                         </div>
 
-                                        <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                        <img className={"coffeeImg"} alt="demos" src={"demos.jpg"}/>
                                     </div>
                                 </div>
 
@@ -115,11 +120,10 @@ class LandingPage extends React.Component {
                                     <div className="coffeeDiv box-shadow">
                                         <div style={{width: "100%", height: "60%"}}>
                                             <text className={"catchUpHeading"}>
-                                                Hangout with the team!
+                                                Recharge yourself
                                             </text>
                                             <text className={"catchUpText"}>
-                                                Schedule happy hours and bond with the team over fun games, activities
-                                                and much more!
+                                                Take a break! Let our instructors guide you to relief from any accumulated stress.
                                             </text>
                                             <button className={"coffeeButton"} theme="pink">
                                                 Get Started
@@ -128,7 +132,7 @@ class LandingPage extends React.Component {
 
                                         </div>
 
-                                        <img className={"coffeeImg"} src={"coffee.jpg"}/>
+                                        <img className={"coffeeImg"} alt="yoga" src={"yoga2.jpg"}/>
                                     </div>
                                 </div>
 
