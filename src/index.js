0e051bc7e3cac07a81e46f858ffda06c924c325c
divya <divyabattu09@gmail.com>
2022-07-23 19:50:23 -0500

    initial commit3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
+import Background from './background.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -9,23 +11,39 @@ function App() {
             <img src={"logo.png"} style={{width : "100%"}}/>
 
 
+<div>
+    <div
+        className="bg_image"
+        style={{
+            backgroundImage: 'url(' + Background + ')',
+            backgroundSize: "cover",
+            height: "100vh"
+        }}>
 
-            <div style={{marginLeft : "100px"}}>
+        <div style={{paddingLeft : "100px"}}>
+        <div>
+            <div className={"coffeeDiv"}>
+                <div style={{width : "100%", height : "60%"}}>
+                    <text className={"catchUpHeading"} >
+                        Catch up over Coffee!
+                    </text>
+                    <text className={"catchUpText"} >
+                        Make the next steps easier! Get to know your peers and build your network.
+                    </text>
 
-                <div style = {{width : "250px",
-            backgroundColor: 'yellow',
-            height: '375px'}}>
-            <div style={{width : "100%", height : "75%"}}>
-
-            </div>
-            <img src={"logo.png"} style={{width : "100%", height : "25%", borderTopLeftRadius : 150 / 2}}/>
                 </div>
 
+                <img className={"coffeeImg"} src={"coffee.jpg"} />
             </div>
 
-
-
         </div>
+        </div>
+
+
+
+    </div>
+</div>
+</div>
     )
 }
 root.render(
